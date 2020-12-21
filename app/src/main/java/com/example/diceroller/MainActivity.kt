75419e45515b2fdbbb914ee4444202cfefe6e9d4
Abdulrafay <abdulrafayn001@gmail.com>
2020-12-22 00:16:21 +0500

    Add dice roll logic and show result on ImageView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,14 +9,34 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var resultImageView:ImageView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton:Button=findViewById(R.id.roll_btn)
-        val resultImageView:ImageView=findViewById(R.id.dice_iv)
+        resultImageView=findViewById(R.id.dice_iv)
 
         rollButton.setOnClickListener{
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // Update the screen with the dice roll
+        when (diceRoll)
+        {
+            1 -> resultImageView.setImageResource(R.drawable.one)
+            2 -> resultImageView.setImageResource(R.drawable.two)
+            3 -> resultImageView.setImageResource(R.drawable.three)
+            4 -> resultImageView.setImageResource(R.drawable.four)
+            5 -> resultImageView.setImageResource(R.drawable.five)
+            6 -> resultImageView.setImageResource(R.drawable.six)
+
         }
     }
 }
