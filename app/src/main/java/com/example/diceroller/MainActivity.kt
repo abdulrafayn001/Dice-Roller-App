039c716d736981264393f426f38dff2c862b7c7b
Abdulrafay <abdulrafayn001@gmail.com>
2020-12-21 13:37:17 +0500

    Add a toast message on roll Button click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton:Button=findViewById(R.id.roll_btn)
+        val resultTextView:TextView=findViewById(R.id.number_tv)
+
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            resultTextView.text="6"
+        }
     }
 }
